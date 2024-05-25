4581ab7377babefb269af398bebd184a171d33e2
Eliza Cohen <elizacohen5@gmail.com>
2024-05-25 09:37:55 -0400

    created header with active navlinks

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,27 @@
 import { NavLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 function Header() {
   return (
-    <nav>
-      <NavLink to="/">Eliza's Appz</NavLink>
-      <NavLink to="/form">+ Add New</NavLink>
-    </nav>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <NavLink className="nav-link" to="/">Eliza's Appz</NavLink>
+            </Typography>
+            <Button color="inherit">
+              <NavLink className="nav-link" to="/form">+ Add New</NavLink>{" "}
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <nav></nav>
+    </>
   );
 }
 
